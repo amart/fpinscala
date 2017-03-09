07fdef40a1b88444306e80d6ae3fac6982e81320
amart <zta@uw.edu>
2017-03-08 18:54:02 -0800

    First pass at chapter 2 exercises

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -35,8 +35,19 @@ object MyModule {
   }
 
   // Exercise 1: Write a function to compute the nth fibonacci number
+  // Q:  does n start from 1 or from 0?
+  // I thought it started at 1 (the first)
+  def fib(n: Int): Int =
+  {
+    def loop(n: Int, i: Int, one: Int, two: Int): Int =
+    {
+      if (n == 0) 0
+      else if (i == n) two
+      else loop(n, i+1, two, one+two)
+    }
 
-  def fib(n: Int): Int = ???
+    loop(n, 1, 0, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
@@ -140,7 +151,17 @@ object PolymorphicFunctions {
 
   // Exercise 2: Implement a polymorphic function to check whether
   // an `Array[A]` is sorted
-  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = ???
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean =
+  {
+    def loop(i: Int, result: Boolean): Boolean =
+    {
+      if (i == 0 || i >= as.length) result
+      else if (!result) result
+      else loop(i+1, gt(as(i-1), as(i)))
+    }
+
+    loop(1, true)
+  }
 
   // Polymorphic functions are often so constrained by their type
   // that they only have one implementation! Here's an example:
@@ -153,13 +174,20 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => ((b: B) => f(a, b))
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    // nope (a: A) => (b: B) => (a, b) => f(a)
+    // nope (a: A, b: B) => (a, b) => f(a)
+    // nope (a: A, b: B) => f(a)
+    // nope (a: A, b: B) => f.uncurried
+    // nope (a: A, b: B) => f(a, b)
+    (a: A, b: B) => f(a)(b)
+    // ??? I thought that f has one argument, a
+    // no, f has two arguments, a and b
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -174,5 +202,5 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    (a: A) => f(g(a))
 }
